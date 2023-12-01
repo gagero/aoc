ad38f0780d04c8ea88df3f45ad735dc9e37cbdd7
Gabor Gero <gaga.gero@gmail.com>
2023-12-01 13:46:47 +0100

    1:1

--- 1.lisp
+++ 1.lisp
@@ -1,14 +1,6 @@
 (defvar *lines* (uiop:read-file-lines "input"))
-(let ((first 0) (last 0) (temp "") (temp-num 0))
-	 (loop for i in *lines* do
-	   (loop for c across i do
-	     (if (parse-integer (string c) :junk-allowed t)
-		 (setf temp (concatenate 'string temp (string c)))
-		 (setf temp ""))
-	     (if (string/= temp "")
-		 (setf temp-num (parse-integer temp))
-		 (progn
-		   (when (not first)
-		     (setf first temp-num))
-		   (setf last temp-num))))
-	       collecting (+ first last)))
+(reduce #'+ (loop for i in *lines* collect
+																	 (parse-integer
+																		(concatenate 'string
+																								 (first (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i))
+																								 (car (last (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i)))))))
