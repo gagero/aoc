c7cd8e599a8b5440ede30fe4e443858d9071b454
gagero <96359986+gagero@users.noreply.github.com>
2023-12-01 17:11:21 +0100

    formatting

--- 1.lisp
+++ 1.lisp
@@ -1,6 +1,6 @@
 (defvar *lines* (uiop:read-file-lines "input"))
 (reduce #'+ (loop for i in *lines* collect
-																	 (parse-integer
-																		(concatenate 'string
-																								 (first (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i))
-																								 (car (last (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i)))))))
+		(parse-integer
+			(concatenate 'string
+				(first (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i))
+				(car (last (cl-ppcre:all-matches-as-strings '(:GREEDY-REPETITION 1 1 :DIGIT-CLASS) i)))))))
